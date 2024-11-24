3f05a42f362fa6dd80590576201659a242c837e4
Agustin Hahn <125411798+agustinhahn@users.noreply.github.com>
2024-11-24 20:27:58 -0300

    problm vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [],
-})
+  build: {
+    target: 'esnext',  // Asegúrate de que esto esté configurado correctamente
+    outDir: 'dist',    // El directorio de salida del build
+    sourcemap: true,   // Habilita el mapa de fuente si es necesario para depurar
+  },
+});
